cdb06041e03e0032033c9eddb8eb50d26271538d
xoviat <xoviat@users.noreply.github.com>
2017-11-11 19:46:15 -0600

    [config] cleanup

--- config.sh
+++ config.sh
@@ -1,25 +1,6 @@
 # Define custom utilities
 # Test for OSX with [ -n "$IS_OSX" ]
 
-function build_github {
-    # Example: build_github fredrik-johansson/arb 2.11.1
-    local path=$1
-    local version=$2
-    local configure_args=${@:3}
-    local name=`basename "$path"`
-    local name_version="${name}-${version}"
-    if [ -e "${name}-stamp" ]; then
-        return
-    fi
-    fetch_unpack "https://github.com/${path}/archive/${version}.tar.gz"
-    ls -r .
-    (cd $name_version \
-        && ./configure --prefix=$BUILD_PREFIX $configure_args \
-        && make \
-        && make install)
-    touch "${name}-stamp"
-}
-
 function pre_build {
     # Any stuff that you need to do before you start building the wheels
     # Runs in the root directory of this repository.
@@ -30,14 +11,7 @@ function pre_build {
         export CC=gcc
         export CXX=gcc
     else
-        wget http://mpir.org/mpir-2.7.0.tar.bz2
-        tar -xf mpir-2.7.0.tar.bz2
-        cd mpir-2.7.0
-        ./configure --enable-gmpcompat --prefix=$BUILD_PREFIX --disable-static --enable-fat
-        make -j4 > /dev/null 2>&1
-        make install
-        cd ..
-
+        build_simple mpir 2.7.0 http://mpir.org/ tar.bz2 --disable-static --enable-fat
         build_simple mpfr 3.1.4 http://www.mpfr.org/mpfr-3.1.4/
         build_simple flint 2.5.2 http://flintlib.org/
         build_github fredrik-johansson/arb 2.11.1
